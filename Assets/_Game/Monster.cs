fae817cb80ab70d9292596b4b8acdbce88ec267a
Adam <achaysmail@gmail.com>
2022-02-07 14:55:09 -0600

    MonsterData ScriptableObject

--- Assets/_Game/Monster.cs
+++ Assets/_Game/Monster.cs
@@ -4,34 +4,14 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    [Header("General Stats")]
     [SerializeField]
-    private string _name;
-    [SerializeField]
-    private MonsterType _monsterType = MonsterType.None;
-    [SerializeField][Range(0,100)]
-    private float _changeToDropItem;
-    [SerializeField][Tooltip("Radius size where monster will see the player")]
-    private float _rangeOfAwareness;
+    private MonsterData _data;
 
-    [Header("Combat Stats")]
-    [SerializeField]
-    private int _damage;
-    [SerializeField]
-    private int _health;
-    [SerializeField]
-    private int _speed;
-
-    [Header("Dialogue")]
-    [SerializeField][Tooltip("Speaks dialogue when entering combat")]
-    [TextArea()]
-    private string _battleCry;
+    public MonsterData Data => _data;
 
     private void Awake()
     {
-        if(_monsterType == MonsterType.Undead)
-        {
-            Debug.Log("Use the holy water!");
-        }
+        Debug.Log("Name: " + _data.Name);
+        Debug.Log("Damage: " + _data.Damage);
     }
 }
---
+++ Assets/_Game/MonsterData.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MonsterData_", menuName = "UnitData/Monster")]
+public class MonsterData : ScriptableObject
+{
+    [Header("General Stats")]
+    [SerializeField]
+    private string _name = "...";
+
+    [SerializeField]
+    private MonsterType _monsterType = MonsterType.None;
+    [SerializeField]
+    [Range(0, 100)]
+    private float _chanceToDropItem = 20;
+    [SerializeField]
+    [Tooltip("Radius size where monster will see the player")]
+    private float _rangeOfAwareness = 10;
+
+    [Header("Combat Stats")]
+    [SerializeField]
+    private int _damage = 1;
+    [SerializeField]
+    private int _health = 1;
+    [SerializeField]
+    private int _speed = 1;
+
+    [Header("Dialogue")]
+    [SerializeField]
+    [Tooltip("Speaks dialogue when entering combat")]
+    [TextArea()]
+    private string _battleCry = "...";
+
+    public string Name => _name;
+    public MonsterType MonsterType => _monsterType;
+    public float ChanceToDropItem => _chanceToDropItem;
+    public float RangeOfAwareness => _rangeOfAwareness;
+
+    public int Damage => _damage;
+    public int Health => _health;
+    public int Speed => _speed;
+
+    public string BattleCry => _battleCry;
+}
