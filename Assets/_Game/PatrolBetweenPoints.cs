3a7ee0f4e1ff58646f9af354f7828f89b8cc8f1b
Adam <achaysmail@gmail.com>
2022-02-07 15:46:14 -0600

    Patrol Points

---
+++ Assets/_Game/PatrolBetweenPoints.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolBetweenPoints : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] _patrolPoints;
+
+    private void OnDrawGizmos()
+    {
+        DrawPoints();
+        DrawPaths();
+    }
+
+    private void DrawPoints()
+    {
+        Gizmos.color = Color.yellow;
+        foreach (Transform point in _patrolPoints)
+        {
+            if (point != null)
+                Gizmos.DrawSphere(point.position, .25f);
+        }
+    }
+
+    private void DrawPaths()
+    {
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < _patrolPoints.Length-1; i++)
+        {
+            Vector3 thisPoint = _patrolPoints[i].position;
+            Vector3 nextPoint = _patrolPoints[i + 1].position;
+            Gizmos.DrawLine(thisPoint, nextPoint);
+        }
+    }
+}
